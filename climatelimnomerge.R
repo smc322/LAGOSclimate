2933352a70dbc237d5efc23af6574017ad2328a9
Sarah Collins <smc322@cornell.edu>
2016-12-05 15:35:07 -0600

    updating for new palmer data and new climate data

--- climatelimnomerge.R
+++ climatelimnomerge.R
@@ -8,13 +8,14 @@
 
 setwd("~/Dropbox/Sarah_Work/Manuscripts/2016_climate_waterqual/Data/Annual_monthly_calculated")
 
+#updated the seasonal ones Dec 5th-6th - data should be same but fixed technical problem with code that was identified when modifying it for palmer data.
 
 hu12.tmean.annual<-read.csv("hu12_tmean_annual.csv", header=T)
 hu12.tmin.annual<-read.csv("hu12_tmin_annual.csv", header=T)
 hu12.tmax.annual<-read.csv("hu12_tmax_annual.csv", header=T)
 hu12.ppt.annual<-read.csv("hu12_ppt_annual.csv", header=T)
-hu12.tmean.season<-read.csv("hu12_tmean_seasonal.csv", header=T)
-hu12.ppt.season<-read.csv("hu12_ppt_seasonal.csv", header=T)
+hu12.tmean.season<-read.csv("hu12_tmean_seasonal_dec2016.csv", header=T)
+hu12.ppt.season<-read.csv("hu12_ppt_seasonal_dec2016.csv", header=T)
 
 
 #separate out relevant columns and merge, add a second year column for x+1 that can be merged with the other year info to get previous years climate data with limno data (this seems like cheating but I was too lazy to figure out how to tell it to add each variable for year x-1 and this should work) Will just need to create a duplicate year column in the limno dataset that's called the same thing as the adjusted one here.
@@ -43,24 +44,21 @@ annseasontpsp$yrx1<-annseasontpsp$year+1
 
 
 #add indicies here too
+phdi<-read.csv("phdi_with_seasonal.csv", header=T)
+
+#pull out seasonal data only, re-name
+phdi.seasons<-phdi[,c(16, 18:22)]
+names(phdi.seasons)<-c("year", "lagoslakeid", "palmer.winter", "palmer.spring", "palmer.summer", "palmer.fall")
+
+#wait to merge phdi into limno data later because it is calculated by lagoslakeid
+
 setwd("~/Dropbox/Sarah_Work/Manuscripts/2016_climate_waterqual/Data/Climate_raw")
 
-
 nao<-read.csv("NAO.csv", header=T)
-pdsi<-read.csv("PDSI 1970-2013 Annual Values by State.csv", header=T)
 enso<-read.csv("ENSO_INDEX.csv", header=T)
 
 ##ENSO and NAO have monthly data, but for now, just aggregate by year.  
 ##PDSI is by state, tried to get that to work but it was a pain in the ass, figure out later...
-
-#hu12.state<-data.lake.specific[,c(20, 26)]
-#hu12.state<-unique(hu12.state)
-#names(pdsi)<-c("state_name", "year", "PDSI.value", "PDSI.anomaly")
-#climate.state<-merge(annseasontpsp, hu12.state, by="hu12_zoneid")
-#climate.pdsi<-merge(climate.state, pdsi, by=c("state_name", "year"))
-
-
-
 
 annual.enso = aggregate(enso[,c("ESPI")], by=list(enso$Year), FUN="mean")
 names(annual.enso)<-c("year", "enso.espi")
@@ -70,19 +68,20 @@ climate.enso<-merge(annseasontpsp, annual.enso, by="year")
 annual.nao = aggregate(nao[,c("NAO_INDEX")], by=list(nao$YEAR), FUN="mean")
 names(annual.nao)<-c("year", "nao.index")
 
-climate.enso.nao<-merge(climate.enso, annual.nao, by="year")
+climate.all<-merge(climate.enso, annual.nao, by="year")
 
-
-#merge in some limno data!
+#add in limno data and palmer data
+#### BELOW STILL NEEDS TO BE MODIFIED FOR UPDATED PALMER DATA... want to add same summer as data, spring and winter of same year, fall of previous year.  need to check column nos and stuff to get it right.  do this after the new climate data are merged correctly.
 head(lakes.limno)
 lakes.limno$yrx1<-lakes.limno$year
-climate.yearx<-climate.enso.nao[,c(1:10,15:20,22:29,34:37,40:41 )]
-climate.yearx1<-climate.enso.nao[,c(2, 11:17, 21,30:34,38:41)]
-names(climate.yearx1)<-c("hu12_zoneid", "September.tmean.x1","October.tmean.x1", "November.tmean.x1", "December.tmean.x1", "tmean.annual.x1", "tmax.annual.x1", "tmin.annual.x1", "tmean.fall.x1", "September.ppt.x1", "October.ppt.x1", "November.ppt.x1", "December.ppt.x1", "precip.annual.x1", "ppt.fall.x1", "yrx1", "enso.espi.x1", "nao.index.x1")
+climate.yearx<-climate.all[,c(2:11,16:21,23:30,35:38,41:44 )]
+climate.yearx1<-climate.all[,c(3, 12:18, 22,31:35,39:44)]
+names(climate.yearx1)<-c("hu12_zoneid", "September.tmean.x1","October.tmean.x1", "November.tmean.x1", "December.tmean.x1", "tmean.annual.x1", "tmax.annual.x1", "tmin.annual.x1", "tmean.fall.x1", "September.ppt.x1", "October.ppt.x1", "November.ppt.x1", "December.ppt.x1", "precip.annual.x1", "ppt.fall.x1", "yrx1", "enso.espi.x1", "nao.index.x1", "PDSI.value.x1", "PDSI.anomaly.x1")
 
 limno.climatex<-merge(lakes.limno, climate.yearx, by=c("hu12_zoneid", "year"), all.x=TRUE, all.y=FALSE)
 limno.allclimate<-merge(limno.climatex, climate.yearx1, by=c("hu12_zoneid", "yrx1"), all.x=TRUE, all.y=FALSE)
 limno.allclimate$yrx1=NULL
+
 
 setwd("~/Dropbox/Sarah_Work/Manuscripts/2016_climate_waterqual/Data/SummaryforShuai")
 write.csv(limno.allclimate, "ClimateCompiled_1.054_initialclim_Oct2016.csv")
--- climatelimnomerge_monthly.R
+++ climatelimnomerge_monthly.R
@@ -8,13 +8,14 @@
 
 setwd("~/Dropbox/Sarah_Work/Manuscripts/2016_climate_waterqual/Data/Annual_monthly_calculated")
 
+#updated the seasonal ones Dec 5th-6th - data should be same but fixed technical problem with code that was identified when modifying it for palmer data.
 
 hu12.tmean.annual<-read.csv("hu12_tmean_annual.csv", header=T)
 hu12.tmin.annual<-read.csv("hu12_tmin_annual.csv", header=T)
 hu12.tmax.annual<-read.csv("hu12_tmax_annual.csv", header=T)
 hu12.ppt.annual<-read.csv("hu12_ppt_annual.csv", header=T)
-hu12.tmean.season<-read.csv("hu12_tmean_seasonal.csv", header=T)
-hu12.ppt.season<-read.csv("hu12_ppt_seasonal.csv", header=T)
+hu12.tmean.season<-read.csv("hu12_tmean_seasonal_dec2016.csv", header=T)
+hu12.ppt.season<-read.csv("hu12_ppt_seasonal_dec2016.csv", header=T)
 
 
 #separate out relevant columns and merge, add a second year column for x+1 that can be merged with the other year info to get previous years climate data with limno data (this seems like cheating but I was too lazy to figure out how to tell it to add each variable for year x-1 and this should work) Will just need to create a duplicate year column in the limno dataset that's called the same thing as the adjusted one here.
@@ -43,11 +44,17 @@ annseasontpsp$yrx1<-annseasontpsp$year+1
 
 
 #add indicies here too
+phdi<-read.csv("phdi_with_seasonal.csv", header=T)
+
+#pull out seasonal data only, re-name
+phdi.seasons<-phdi[,c(16, 18:22)]
+names(phdi.seasons)<-c("year", "lagoslakeid", "palmer.winter", "palmer.spring", "palmer.summer", "palmer.fall")
+
+#wait to merge phdi into limno data later because it is calculated by lagoslakeid
+
 setwd("~/Dropbox/Sarah_Work/Manuscripts/2016_climate_waterqual/Data/Climate_raw")
 
-
 nao<-read.csv("NAO.csv", header=T)
-pdsi<-read.csv("PDSI 1970-2013 Annual Values by State.csv", header=T)
 enso<-read.csv("ENSO_INDEX.csv", header=T)
 
 ##ENSO and NAO have monthly data, but for now, just aggregate by year.  
@@ -61,14 +68,10 @@ climate.enso<-merge(annseasontpsp, annual.enso, by="year")
 annual.nao = aggregate(nao[,c("NAO_INDEX")], by=list(nao$YEAR), FUN="mean")
 names(annual.nao)<-c("year", "nao.index")
 
-climate.enso.nao<-merge(climate.enso, annual.nao, by="year")
+climate.all<-merge(climate.enso, annual.nao, by="year")
 
-hu12.state<-data.lake.specific[,c(20, 26)]
-hu12.state<-unique(hu12.state)
-climate.enso.nao.state<-merge(climate.enso.nao, hu12.state, by="hu12_zoneid")
-names(pdsi)<-c("state_name", "year", "PDSI.value", "PDSI.anomaly")
-climate.all<-merge(climate.enso.nao.state, pdsi, by=c("state_name", "year"))
-
+#add in limno data and palmer data
+#### BELOW STILL NEEDS TO BE MODIFIED FOR UPDATED PALMER DATA... want to add same summer as data, spring and winter of same year, fall of previous year.  need to check column nos and stuff to get it right.  do this after the new climate data are merged correctly.
 head(lakes.limno)
 lakes.limno$yrx1<-lakes.limno$year
 climate.yearx<-climate.all[,c(2:11,16:21,23:30,35:38,41:44 )]
