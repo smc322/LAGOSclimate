55ce5e4929cb430ccf30b50ac4dcc701095d3721
Sarah Collins <smc322@cornell.edu>
2017-04-18 16:36:42 -0500

    Adding code to subset lake weights from new analysis by a mean + sd of the RMSE to get rid of any lakes with particularly high error

--- RMSE_histos.R
+++ RMSE_histos.R
@@ -35,3 +35,41 @@ hist(s.l3.005$RMSE, add=T, col="green")
 
 #looks pretty similar regardless of lambda3 choice (.003 vs .005 for everything but secchi, also .008 for that because it seemed relevant based on Shuai's summary), totally overlapping distributions and more or less same mean and sd for rmse.  Perhaps do a cutoff of one SD RMSE for the subset of lakes with data for JF?
 
+##make cutoff of RMSE plus one SD for eliminating lakes with very high error from clustering analysis
+
+
+##dataframes of regression weights for lakes that had data (from subsettingforclusters_dec2016.R)
+setwd("~/Dropbox/Sarah_Work/Manuscripts/2017_LAGOSClimateWaterqual/Data/RegressionWeights_April2017")
+
+n.datalakes<-read.csv("TN_lambda3_003_datalakes.csv", header=T)
+n.rmse.cutoff<-mean(n.l3.003$RMSE)+sd(n.l3.003$RMSE)
+n.rmse.trimmed<-n.l3.003[n.l3.003$RMSE<n.rmse.cutoff,]
+n.lakelist<-data.frame(n.rmse.trimmed[,1])
+names(n.lakelist)<-"lagoslakeid"
+n.weights.rmse<-merge(n.datalakes, n.lakelist, by="lagoslakeid", all.x=F, all.y=T)
+write.csv(n.weights.rmse, "TN_lambda3_003_rmselakes.csv")
+
+p.datalakes<-read.csv("TP_lambda3_003_datalakes.csv", header=T)
+p.rmse.cutoff<-mean(p.l3.003$RMSE)+sd(p.l3.003$RMSE)
+p.rmse.trimmed<-p.l3.003[p.l3.003$RMSE<p.rmse.cutoff,]
+p.lakelist<-data.frame(p.rmse.trimmed[,1])
+names(p.lakelist)<-"lagoslakeid"
+p.weights.rmse<-merge(p.datalakes, p.lakelist, by="lagoslakeid", all.x=F, all.y=T)
+write.csv(p.weights.rmse, "TP_lambda3_003_rmselakes.csv")
+
+chla.datalakes<-read.csv("Chla_lambda3_003_datalakes.csv", header=T)
+chla.rmse.cutoff<-mean(c.l3.003$RMSE)+sd(c.l3.003$RMSE)
+chla.rmse.trimmed<-c.l3.003[c.l3.003$RMSE<chla.rmse.cutoff,]
+chla.lakelist<-data.frame(chla.rmse.trimmed[,1])
+names(chla.lakelist)<-"lagoslakeid"
+chla.weights.rmse<-merge(chla.datalakes, chla.lakelist, by="lagoslakeid", all.x=F, all.y=T)
+write.csv(chla.weights.rmse, "Chla_lambda3_003_rmselakes.csv")
+
+
+sec.datalakes<-read.csv("Secchi_lambda3_003_datalakes.csv", header=T)
+sec.rmse.cutoff<-mean(s.l3.003$RMSE)+sd(s.l3.003$RMSE)
+sec.rmse.trimmed<-s.l3.003[s.l3.003$RMSE<sec.rmse.cutoff,]
+sec.lakelist<-data.frame(sec.rmse.trimmed[,1])
+names(sec.lakelist)<-"lagoslakeid"
+sec.weights.rmse<-merge(sec.datalakes, sec.lakelist, by="lagoslakeid", all.x=F, all.y=T)
+write.csv(sec.weights.rmse, "Secchi_lambda3_003_rmselakes.csv")
